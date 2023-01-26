9a0cfcefa2745abf9fa9181dbc41553fd2f0047b
julio cesar <jcmcf2012@gmail.com>
2023-01-26 11:46:10 -0300

    performance: remove logic from main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,17 @@
-import { SetStateAction, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
+import Input from "./components/Input";
 import { ICityObject } from "./typeSafe/ICityObject";
 
-const fetchCityTemperature = async (cityName: string) => {
-  const websiteName = "https://api.openweathermap.org/data/2.5/weather";
-  const cityQuery = `?q=${cityName}`;
-  const apiKey = "&appid=aaa4b55b9a2bbd6e73ef18b8bbff7416";
-  const metric = "&units=metric";
-
-  const city = await fetch(`${websiteName}${cityQuery}${apiKey}${metric}`, {
-    mode: "cors",
-  });
-
-  const cityResult = await city.json();
-  return cityResult;
-};
-
-const searchCity = async (
-  city: string,
-  setCityCard: {
-    (value: SetStateAction<ICityObject | null>): void;
-    (arg0: any): void;
-  }
-) => {
-  const cityTemperature = await fetchCityTemperature(city);
-  setCityCard(cityTemperature);
-};
-
 function App() {
-  const [city, setCity] = useState("");
   const [cityCard, setCityCard] = useState<ICityObject | null>(null);
 
   return (
     <div className="App">
       <h1>WEATHER API</h1>
       <p>Busque aqui a temperatura de qualquer cidade do mundo</p>
-      <div className="button__container">
-        <input
-          placeholder="NOME DA CIDADE"
-          onChange={(event) => {
-            setCity(event.target.value);
-          }}
-        ></input>
-        <button
-          onClick={() => {
-            searchCity(city, setCityCard);
-          }}
-        >
-          Procurar Temperatura
-        </button>
-      </div>
+      <Input setCityCard={setCityCard}></Input>
       {cityCard !== null && <Card city={cityCard}></Card>}
     </div>
   );
